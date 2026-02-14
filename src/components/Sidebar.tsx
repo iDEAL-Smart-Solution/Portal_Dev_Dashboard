4ddf9a2146a775605ff4272326e87b92ac74d2a1
Bigboss298 <abdullahbisiriyu988@gmail.com>
2026-02-14 18:27:40 +0100

    fix

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import logo from '../assets/logo.jpg';
+import logo from '../assets/logo.png';
 
 interface SidebarProps {
   onLogout: () => void;
